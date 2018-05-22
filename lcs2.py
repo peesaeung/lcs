ac41d8eb052a883147c52d00fc9408e62d0e2749
peesaeung <peeranut_saeung@hotmail.com>
2018-05-22 21:51:06 +0700

    no message

--- lcs2.py
+++ lcs2.py
@@ -40,17 +40,17 @@ def table(s,b,v,w,mat,mis,ind):
             if s[i][j]==wind:
                 b[i][j]=b[i][j]+'←'            
             if v[i-1]==w[j-1]:
-                if s[i][j]==sbuf1 or s[i][j]==sbuf2:
+                if s[i][j]==sbuf1:
                     b[i][j]=b[i][j]+'↖︎'
+            elif s[i][j]==sbuf2:
+                b[i][j]=b[i][j]+'↖︎'
     return 
 
 def printglcs(s,b,v,w):
     k1=[]*len(w)
-    for p in range(1,len(w)+1):
-        if s[len(v)][p]==max(s[len(v)]):        
-            i=len(v)
-            j=p
-            align(b,v,s[len(v)][p],i,j,k1)
+    i=len(v)
+    j=len(w)
+    align(b,v,w,s[len(v)][len(w)],i,j,k1)
     return k1
 
 def printllcs(s,b,v,w):
@@ -69,25 +69,93 @@ def printllcs(s,b,v,w):
         if p1[p]==max(p1):        
             i=p2[p]
             j=p3[p]
-            align(b,v,p1[p],i,j,k1)
+            align(b,v,w,p1[p],i,j,k1)
     return k1
 
-def align(b,v,p,i,j,k1):
-    k=''
-    k2=[[],[]]
+#def printglcs(s,b,v,w):
+ #   k1=[]*len(w)
+  #  i=len(v)
+   # j=len(w)
+    #for t in range(0,3):
+     #   align(b,v,w,s[len(v)][len(w)],i,j,t,k1)
+#    return k1
+
+#def printllcs(s,b,v,w):
+ #   p1=[]*len(v)*len(w)
+  #  p2=[]*len(v)*len(w)
+   # p3=[]*len(v)*len(w)
+    #for pi in range(1,len(v)+1):
+     #   for pj in range(1,len(w)+1):
+      #      if s[pi][pj]==max(s[pi]):
+       #         p1.append(max(s[pi]))
+        #        p2.append(pi)
+         #       p3.append(pj)
+#    k1=[]*len(p1)
+    
+ #   for p in range(0,len(p1)):
+  #      if p1[p]==max(p1):        
+   #         i=p2[p]
+    #        j=p3[p]
+     #       align(b,v,w,p1[p],i,j,k1)
+#    return k1
+
+def align(b,v,w,p,i,j,k1):
+    k=l=''
+    k2=[[],[],[]]
     k2[0]=p
     while i > 0 and j > 0:
         if "↖︎" in b[i][j]:
             k=v[i-1]+k
+            l=w[j-1]+l
             i-=1
             j-=1
         elif "↑" in b[i][j]:
+            k=v[i-1]+k
+            l='_'+l
             i-=1
         else:
+            k='_'+k
+            l=w[j-1]+l
             j-=1
     k2[1].append(k)
+    k2[2].append(l)
     k1.append(k2)
     return
+
+#def align(b,v,w,p,i,j,t,k1):
+ #   k=l=''
+  #  k2=[[],[],[]]
+   # k2[0]=p
+    #while i > 0 and j > 0:
+     #   if len(b[i][j])==1:
+      #      tx=0
+       # elif len(b[i][j])==2 and t==0:
+        #    tx=0
+#        elif len(b[i][j])==2 and t==1:
+  #          tx=1
+   #     elif len(b[i][j])==3 and t==0:
+    #        tx=0
+     #   elif len(b[i][j])==3 and t==1:
+      #      tx=1
+       # elif len(b[i][j])==3 and t==2:
+        #    tx=2    
+#        if "↖︎" in b[i][j][tx]:
+ #           k=v[i-1]+k
+  #          l=w[j-1]+l
+   #         i-=1
+    #        j-=1
+     #   elif "↑" in b[i][j][tx]:
+      #      k=v[i-1]+k
+       #     l='_'+l
+        #    i-=1
+        #else:
+         #   k='_'+k
+          #  l=w[j-1]+l
+           # j-=1
+#    k2[1].append(k)
+ #   k2[2].append(l)
+  #  k1.append(k2)
+   # return
 
 X1="GCATGCTA"
 Y1="GATTACAA"
